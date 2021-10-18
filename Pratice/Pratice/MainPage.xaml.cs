71d5e16f1309ef42f234dd90056cd0fb51317184
nammac09 <87338649+nammac09@users.noreply.github.com>
2021-10-18 14:42:35 +0700

    T

---
+++ Pratice/Pratice/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Pratice
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private List<Icon> Icons;
+        private ObservableCollection<Item> items;
+        public MainPage()
+        {
+            Icons = new List<Icon>();
+            Icons.Add(new Icon { IconPath = "Assets/1.jpg" });
+            Icons.Add(new Icon { IconPath = "Assets/2.jpg" });
+            Icons.Add(new Icon { IconPath = "Assets/3.jpg" });
+            Icons.Add(new Icon { IconPath = "Assets/4.jpg" });
+            Icons.Add(new Icon { IconPath = "Assets/5.jpg" });
+            Icons.Add(new Icon { IconPath = "Assets/6.jpg" });
+            Icons.Add(new Icon { IconPath = "Assets/7.jpg" });
+            Icons.Add(new Icon { IconPath = "Assets/8.jpg" });
+            Icons.Add(new Icon { IconPath = "Assets/9.jpg" });
+            Icons.Add(new Icon { IconPath = "Assets/10.jpg" });
+
+            items = new ObservableCollection<Item>();
+            items.Add(new Item { Pruduct = "Meo", Description = "Hi hi", ImgPath = "Assets/5.jpg" });
+            this.InitializeComponent();
+        }
+
+        private void OrderButton_Click(object sender, RoutedEventArgs e)
+        {
+            string imgpath = ((Icon)MyCombobox.SelectedValue).IconPath;
+            items.Add(new Item { Pruduct = ProductReults.Text, Description = DescriptionReults.Text, ImgPath = imgpath });
+
+            ProductReults.Text = "";
+            DescriptionReults.Text = "";
+            MyCombobox.SelectedIndex = -1;
+
+            ProductReults.Focus(FocusState.Programmatic);
+
+
+        }
+    }
+}
---
+++ Pratice/Pratice/models/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratice.models
+{
+    class Item
+    {
+        public string Pruduct { get; set; }
+        public string Description { get; set; }
+        public string ImgPath { get; set; }
+    }
+}
