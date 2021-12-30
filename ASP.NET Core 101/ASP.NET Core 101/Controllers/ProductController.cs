a17b044c22de2e4887dc0220f600d0484ec0ccf8
nammac09 <87338649+nammac09@users.noreply.github.com>
2021-12-31 00:44:42 +0700

    new

---
+++ ASP.NET Core 101/ASP.NET Core 101/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using ASP.NET_Core_101.Services;
+using ASP.NET_Core_101.Models;
+using System.Collections.Generic;
+
+namespace ASP.NET_Core_101.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+
+        [HttpPatch]
+        public ActionResult Patch([FromBody] RatingRequest request)
+        {
+            ProductService.AddRating(request.ProductId, request.Rating);
+
+            return Ok();
+        }
+
+        public class RatingRequest
+        {
+            public string ProductId { get; set; }
+            public int Rating { get; set; }
+        }
+    }
+}
---
+++ ASP.NET Core 101/ASP.NET Core 101/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASP.NET_Core_101.Services;
+using ASP.NET_Core_101.Models;
+
+namespace ASP.NET_Core_101.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger,
+            JsonFileProductService productService)
+        {
+            _logger = logger;
+            ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+        public IEnumerable<Product> Products { get; private set; }
+
+        public void OnGet()
+        {
+            Products = ProductService.GetProducts();
+        }
+    }
+}
