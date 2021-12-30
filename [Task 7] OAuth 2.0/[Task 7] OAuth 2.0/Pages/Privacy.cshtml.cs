07eb0c253a5970e352a09aa586e49ca115befb90
nammac09 <87338649+nammac09@users.noreply.github.com>
2021-12-31 00:48:16 +0700

    newww

---
+++ [Task 7] OAuth 2.0/[Task 7] OAuth 2.0/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _Task_7__OAuth_2._0.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
